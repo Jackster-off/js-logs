bc7c211a0e307ceb40e6ceb9567e8738750cae4f
Jackster <126988682+Jackster-off@users.noreply.github.com>
2024-02-24 04:49:00 +0000

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+lua54 'yes' 
+fx_version 'cerulean'
+game 'gta5'
+version '1.0.0'
+author '.jackster.' -- Dont change ❤
+description 'Anti pistol whip'
+lua54 'on'
+client_scripts {
+	'client.lua'
+}
